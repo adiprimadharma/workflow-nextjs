9a7dc827a1ab9dccc2f8f1b11dd5ec3de58b7632
Adi Primadharma <adi.primadharma@tokopedia.com>
2021-10-08 09:43:18 +0700

    wew

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         </h1>
 
         <p className="description">
-          Test Get started by editing <code>pages/index.js</code>
+          Get started by editing <code>pages/index.js</code>
         </p>
 
         <div className="grid">
